9535a140a81192169e60f880d8d4d310393b47e1
Karin.T <Karin.T@172.19.14.146>
2018-07-26 01:07:26 +0900

    ロック解除システム（修正必要）

---
+++ src/filter/NearestNeighbor2.java
@@ -0,0 +1,65 @@
+package filter;
+
+
+import java.io.FileReader;
+import java.io.StreamTokenizer;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+//import filter.MachineLearning2;
+//import filter.NearestNeighbor;
+
+public class NearestNeighbor2 {
+
+	List<Map.Entry<Integer, Integer[]>> patterns = 
+            new ArrayList<Map.Entry<Integer, Integer[]>>();
+/*
+    public static void main(String[] args) {
+        new MachineLearning2(new NearestNeighbor());
+    }
+*/
+	public void learn(int cls, ArrayList<Integer> data) {//最近傍法を行うためにサンプルを集める
+		int size = data.size();
+		Integer[] sample=data.toArray(new Integer[size]);
+        patterns.add(new AbstractMap.SimpleEntry(cls, sample));
+    }
+
+    public int trial(ArrayList<Integer> dsts) {//識別
+        int cls = 0;
+        int length=0;
+        int size = dsts.size();
+        Integer[] data=dsts.toArray(new Integer[size]);
+        
+        
+        //一番近いパターンを求める
+        double mindist = Double.POSITIVE_INFINITY;
+        for (Map.Entry<Integer, Integer[]> entry : patterns) {
+            Integer[] ss = entry.getValue();
+            
+            //登録した軌跡と解除する軌跡の配列が異なる場合、短い方の配列の長さに合わせる
+            if (ss.length - data.length>0) {
+                length=data.length;
+            }else {
+            	length=ss.length;
+            }
+            
+            
+            //データ間の距離を求める
+            double dist = 0;
+            for (int i = 0; i < length; ++i) {
+                dist += (ss[i] - data[i]) * (ss[i] - data[i]);
+                /*System.out.println("軌跡の距離を求めてる");
+                System.out.println(ss[i]);
+                System.out.println(data[i]);*/
+            }
+            
+            if (mindist > dist) {
+                mindist = dist;
+                cls =entry.getKey(); 
+            }
+        }
+        return cls;
+    }
+}
